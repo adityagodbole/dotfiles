a206f24816973d002263a9bacabe893ae3feab2c
Aditya Godbole <aditya.a.godbole@gmail.com>
2012-07-13 00:09:39 +0530

    fish config

---
+++ config.fish
@@ -0,0 +1,38 @@
+function fish_prompt -d "Write out the prompt"
+  printf '%s%s%s>%s ' (set_color $fish_color_cwd) (basename (pwd)) (set_color normal) (get_git_prompt)
+end
+
+function get_git_branch
+	git branch | awk '/^\*/ { print $2 }'
+end
+function get_git_dirty
+	git diff --quiet; or echo '*'
+end
+function get_git_prompt
+	git branch > /dev/null ^&1; or return 1
+	echo \ (get_git_branch)(get_git_dirty)\)
+end
+
+set -x NODE_PATH /usr/local/lib/node_modules
+set -x LC_ALL C
+
+function vim
+	/Applications/MacVim.app/Contents/MacOS/Vim $argv
+end
+
+function mvim 
+	vim -g $argv
+end
+
+function e
+	mvim $argv
+end
+
+function o
+	open $argv
+end
+
+set -x PATH $PATH ~/.rvm/bin
+set -x EDITOR vim
+
+rvm ^&1 > /dev/null
